a3cb375fa1e80f1deafee46aafa2df970c32171e
charakuri8 <charakuri8@gmail.com>
2021-06-06 20:56:35 +0900

    20210606 shimamura commit

---
+++ lib/models/apply_work.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hatarakujikan_web/models/breaks.dart';
+
+class ApplyWorkModel {
+  String _id;
+  String _workId;
+  String _groupId;
+  String _userId;
+  DateTime _startedAt;
+  DateTime _endedAt;
+  List<BreaksModel> breaks;
+  DateTime _createdAt;
+
+  String get id => _id;
+  String get workId => _workId;
+  String get groupId => _groupId;
+  String get userId => _userId;
+  DateTime get startedAt => _startedAt;
+  DateTime get endedAt => _endedAt;
+  DateTime get createdAt => _createdAt;
+
+  ApplyWorkModel.fromSnapshot(DocumentSnapshot snapshot) {
+    _id = snapshot.data()['id'];
+    _workId = snapshot.data()['workId'];
+    _groupId = snapshot.data()['groupId'];
+    _userId = snapshot.data()['userId'];
+    _startedAt = snapshot.data()['startedAt'].toDate();
+    _endedAt = snapshot.data()['endedAt'].toDate();
+    breaks = _convertBreaks(snapshot.data()['breaks']) ?? [];
+    _createdAt = snapshot.data()['createdAt'].toDate();
+  }
+
+  List<BreaksModel> _convertBreaks(List breaks) {
+    List<BreaksModel> converted = [];
+    for (Map data in breaks) {
+      converted.add(BreaksModel.fromMap(data));
+    }
+    return converted;
+  }
+}
--- lib/models/group.dart
+++ lib/models/group.dart
@@ -4,17 +4,23 @@ class GroupModel {
   String _id;
   String _name;
   String _adminUserId;
+  int _usersNum;
+  bool _workSecurity;
   DateTime _createdAt;
 
   String get id => _id;
   String get name => _name;
   String get adminUserId => _adminUserId;
+  int get usersNum => _usersNum;
+  bool get workSecurity => _workSecurity;
   DateTime get createdAt => _createdAt;
 
   GroupModel.fromSnapshot(DocumentSnapshot snapshot) {
     _id = snapshot.data()['id'];
     _name = snapshot.data()['name'];
     _adminUserId = snapshot.data()['adminUserId'];
+    _usersNum = snapshot.data()['usersNum'];
+    _workSecurity = snapshot.data()['workSecurity'];
     _createdAt = snapshot.data()['createdAt'].toDate();
   }
 }
