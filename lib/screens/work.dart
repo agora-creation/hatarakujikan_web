4ba303bd565296ec88a40c6dd7f2d9959f330d01
charakuri8 <charakuri8@gmail.com>
2021-06-07 22:56:12 +0900

    20210607 shimamura commit

--- lib/screens/work.dart
+++ lib/screens/work.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hatarakujikan_web/providers/group.dart';
 import 'package:hatarakujikan_web/widgets/custom_admin_scaffold.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class WorkScreen extends StatelessWidget {
   static const String id = 'work';
@@ -13,7 +14,136 @@ class WorkScreen extends StatelessWidget {
     return CustomAdminScaffold(
       groupProvider: groupProvider,
       selectedRoute: id,
-      body: Center(child: Text('勤怠')),
+      body: WorkTable(),
     );
   }
+}
+
+class WorkTable extends StatefulWidget {
+  @override
+  _WorkTableState createState() => _WorkTableState();
+}
+
+class _WorkTableState extends State<WorkTable> {
+  List<Employee> employees = <Employee>[];
+  EmployeeDataSource employeeDataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(employeeData: employees);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SfDataGrid(
+      source: employeeDataSource,
+      columnWidthMode: ColumnWidthMode.fill,
+      columns: <GridColumn>[
+        GridTextColumn(
+          columnName: 'id',
+          label: Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: Text(
+              'ID',
+            ),
+          ),
+        ),
+        GridTextColumn(
+          columnName: 'name',
+          label: Container(
+            padding: EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Text('Name'),
+          ),
+        ),
+        GridTextColumn(
+          columnName: 'designation',
+          label: Container(
+            padding: EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Text(
+              'Designation',
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+        GridTextColumn(
+          columnName: 'salary',
+          label: Container(
+            padding: EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Text('Salary'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee(10001, 'James', 'Project Lead', 20000),
+      Employee(10002, 'Kathryn', 'Manager', 30000),
+      Employee(10003, 'Lara', 'Developer', 15000),
+      Employee(10004, 'Michael', 'Designer', 15000),
+      Employee(10005, 'Martin', 'Developer', 15000),
+      Employee(10006, 'Newberry', 'Developer', 15000),
+      Employee(10007, 'Balnc', 'Developer', 15000),
+      Employee(10008, 'Perry', 'Developer', 15000),
+      Employee(10009, 'Gable', 'Developer', 15000),
+      Employee(10010, 'Grimes', 'Developer', 15000)
+    ];
+  }
+}
+
+class EmployeeDataSource extends DataGridSource {
+  EmployeeDataSource({@required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(
+                columnName: 'id',
+                value: e.id,
+              ),
+              DataGridCell<String>(
+                columnName: 'name',
+                value: e.name,
+              ),
+              DataGridCell<String>(
+                columnName: 'designation',
+                value: e.designation,
+              ),
+              DataGridCell<int>(
+                columnName: 'salary',
+                value: e.salary,
+              ),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8.0),
+        child: Text(e.value.toString()),
+      );
+    }).toList());
+  }
+}
+
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
+  final int id;
+  final String name;
+  final String designation;
+  final int salary;
 }
