1f1e1543109b096f941cde9325501774e6d1995a
agora.creation <apple@agora-c.com>
2021-09-08 17:37:21 +0900

    20210908 agora commit

--- lib/screens/work_shift.dart
+++ lib/screens/work_shift.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hatarakujikan_web/helpers/style.dart';
 import 'package:hatarakujikan_web/providers/group.dart';
 import 'package:hatarakujikan_web/widgets/custom_admin_scaffold.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class WorkShiftScreen extends StatelessWidget {
   static const String id = 'work_shift';
@@ -13,7 +15,50 @@ class WorkShiftScreen extends StatelessWidget {
     return CustomAdminScaffold(
       groupProvider: groupProvider,
       selectedRoute: id,
-      body: Container(),
+      body: WorkShiftTable(
+        groupProvider: groupProvider,
+      ),
+    );
+  }
+}
+
+class WorkShiftTable extends StatefulWidget {
+  final GroupProvider groupProvider;
+
+  WorkShiftTable({@required this.groupProvider});
+
+  @override
+  _WorkShiftTableState createState() => _WorkShiftTableState();
+}
+
+class _WorkShiftTableState extends State<WorkShiftTable> {
+  final CalendarController _calendarController = CalendarController();
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController.view = CalendarView.timelineMonth;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'シフト表',
+          style: kAdminTitleTextStyle,
+        ),
+        SizedBox(height: 16.0),
+        Expanded(
+          child: SfCalendar(
+            showDatePickerButton: true,
+            controller: _calendarController,
+            allowViewNavigation: false,
+            todayHighlightColor: Colors.lightBlue,
+          ),
+        ),
+      ],
     );
   }
 }
