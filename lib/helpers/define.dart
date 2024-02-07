e8aafc1eb79066d1ca3c9abafe9be7100b4e4c78
Shimamura Yuta <shimamura@agora-c.com>
2024-02-07 14:45:39 +0900

    20240207 commit

--- lib/helpers/define.dart
+++ lib/helpers/define.dart
@@ -6,7 +6,7 @@ const List<String> pdfTemplates = ['ひろめカンパニー用レイアウト',
 const List<String> workStates = ['通常勤務', '直行/直帰', 'テレワーク'];
 const List<String> workShiftStates = ['勤務予定', '欠勤', '特別休暇', '有給休暇', '代休'];
 
-DateTime kMonthFirstDate = DateTime(DateTime.now().year - 1);
+DateTime kMonthFirstDate = DateTime(2018);
 DateTime kMonthLastDate = DateTime(DateTime.now().year + 1);
 DateTime kDayFirstDate = DateTime.now().subtract(Duration(days: 365));
 DateTime kDayLastDate = DateTime.now().add(Duration(days: 365));
